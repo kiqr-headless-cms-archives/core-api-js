bd20fd9bd1d4ceaf91dc79ed4cce5897d63476f6
kjellberg <2277443+kjellberg@users.noreply.github.com>
2023-03-18 17:23:49 +0100

    Generate SDK

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,4 +1,5 @@
 /* tslint:disable */
 /* eslint-disable */
+export * from './ProjectsApi';
 export * from './SchemasApi';
 export * from './UserApi';
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,9 +1,13 @@
 /* tslint:disable */
 /* eslint-disable */
 export * from './ContentType';
+export * from './CreateProjectRequest';
 export * from './CreateSchemaRequest';
 export * from './Environment';
 export * from './Field';
+export * from './PaginationMeta';
 export * from './Project';
+export * from './ProjectsCollection';
+export * from './ProjectsCollectionMeta';
 export * from './Schema';
 export * from './User';
